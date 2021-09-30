9864c374a564029428cf1bbabef67a9fc18171ee
justinl <cas022101@coderacademy.edu.au>
2021-09-30 11:29:47 +1000

    Fixed add high score method, working

--- src/high_score.rb
+++ src/high_score.rb
@@ -1,30 +1,30 @@
 require 'yaml'
 
 def add_high_score(name, home_runs) 
+
+    # retrieve current high scores from yaml file
     high_scores = YAML.load(File.read("high_scores.yml"))
-    # p high_scores
 
-    if home_runs == 0
-        
-    elsif home_runs >= high_scores[0][:score].to_i
-        new_high_scores = high_scores.unshift({name: name.to_s, score: home_runs.to_i})
-    else
-        i = 0
-        until i == high_scores.length.to_i
-            x = high_scores[i][:score].to_i
-            y = high_scores[i.to_i + 1][:score].to_i
+    # create new record from recent game
+    new_record = {name: name.to_s, score: home_runs.to_i}
 
-            if (home_runs <= x) && (home_runs >= y)
-                puts i
-                new_high_scores =  high_scores.insert(i, {name: name.to_s, score: home_runs.to_i})
-            end
-            i += 1
+    # add new record to start of current high scores array
+    high_scores.unshift(new_record)
+
+    # loop through current high scores, sorting the new record to the correct place, highest first
+    i = 0
+    while i < high_scores.length - 1
+
+        if high_scores[i][:score] < high_scores[i + 1][:score]
+            high_scores[i], high_scores[i + 1] = high_scores[i + 1], high_scores[i]
         end
+        i += 1
     end
 
+    # write new high scores array with new record to yaml file
     File.open("high_scores.yml", "w") { |file|
         file.write(high_scores.to_yaml)
     }
 end
 
-add_high_score("Jeremy", 2)
+add_high_score("Jeremy", 3)
